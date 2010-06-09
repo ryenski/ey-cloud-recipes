511a8731b4d453351af6075332df8ea206f240c5
Scott M. Likens <slikens@engineyard.com>
2010-06-09 15:41:42 -0700

    - Update DNapi and try and bring in Postgres a bit ey-cloudy.
    Signed-off-by: Scott M. Likens <slikens@engineyard.com>

---
+++ cookbooks/postgres/recipes/database.rb
@@ -0,0 +1,20 @@
+# Setup database.yml
+dbtype = 'postgresql'
+
+node.engineyard.apps.each do |app|
+
+  template "/data/#{app.name}/shared/config/database.yml" do
+    owner node.engineyard.environment.ssh_username
+    group node.engineyard.environment.ssh_username
+    mode 0655
+    source "database.yml.erb"
+    variables({
+      :dbuser => node.engineyard.environment.ssh_username,
+      :dbpass => node.engineyard.environment.ssh_password,
+      :dbname => app.database_name,
+      :dbhost => node.engineyard.environment.db_host,
+      :dbtype => dbtype,
+      :slaves => node.engineyard.environment.db_slaves_hostnames
+    })
+  end
+end
---
+++ cookbooks/postgres/recipes/server.rb
@@ -0,0 +1,36 @@
+postgres_version = '8.3'
+postgres_root    = '/var/lib/postgresql'
+
+ey_cloud_report "postgress install" do
+  message "processing postgresql #{postgres_version}"
+end
+
+require_recipe 'postgres::server_setup'
+require_recipe 'postgres::server_configure'
+
+runlevel "postgresql-#{postgres_version}" do
+  action :add
+end
+
+execute "start-postgres" do
+  command "/etc/init.d/postgresql-#{postgres_version} restart"
+  action :run
+  not_if "/etc/init.d/postgresql-#{postgres_version} status | grep -q start"
+end
+
+username = node.engineyard.ssh_username
+password = node.engineyard.ssh_password
+
+psql "create-db-user-#{username}" do
+  sql "create user #{username} with encrypted password '#{password}'"
+  sql_not_if :sql => 'SELECT * FROM pg_roles',
+             :assert => "grep -q #{username}"
+end
+
+node.engineyard.apps.each do |app|
+  createdb app.database_name do
+    owner username
+  end
+end
+
+require_recipe 'postgres::eybackup'
