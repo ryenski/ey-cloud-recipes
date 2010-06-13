3b937ec4bd6ea57e61f072b58b2af3399e81c705
Scott M. Likens <slikens@engineyard.com>
2010-06-13 15:03:36 -0700

    - Back on track with replication me hopes!
    Signed-off-by: Scott M. Likens <slikens@engineyard.com>

--- cookbooks/postgres/recipes/setup_slave.rb
+++ cookbooks/postgres/recipes/setup_slave.rb
@@ -2,6 +2,56 @@ postgres_root = '/db/postgresql'
 postgres_version = '8.3'
 
 if ['db_slave'].include?(node[:instance_role])
+
+  template "#{postgres_root}/#{postgres_version}/postgresql.conf" do
+    source "postgresql.conf.erb"
+    owner "postgres"
+    group "postgres"
+    backup 0
+    mode 0600
+
+    variables(
+      :sysctl_shared_buffers => node[:sysctl_shared_buffers],
+      :shared_buffers => node[:shared_buffers],
+      :max_fsm_pages => node[:max_fsm_pages],
+      :max_fsm_relations => node[:max_fsm_relations],
+      :maintenance_work_mem => node[:maintenance_work_mem],
+      :work_mem => node[:work_mem],
+      :max_stack_depth => node[:max_stack_depth],
+      :effective_cache_size => node[:effective_cache_size],
+      :default_statistics_target => node[:default_statistics_target],
+      :logging_collector => node[:logging_collector],
+      :log_rotation_age => node[:log_rotation_age],
+      :log_rotation_size => node[:log_rotation_size],
+      :checkpoint_timeout => node[:checkpoint_timeout],
+      :checkpoint_segments => node[:checkpoint_segments],
+      :wal_buffers => node[:wal_buffers],
+      :wal_writer_delay => node[:wal_writer_delay],
+      :postgres_root => postgres_root,
+      :postgres_version => postgres_version
+   )
+  end
+
+  remote_file "#{postgres_root}/#{postgres_version}/recovery.conf" do
+    source "recovery.conf"
+    woner "postgres"
+    group "postgres"
+    mode 0600
+    backup 0
+  end
+
+  template "#{postgres_root}/#{postgres_version}/pg_hba.conf" do
+    owner 'postgres'
+    group 'postgres'
+    backup 0
+    mode 0600
+    source "pg_hba.conf.erb"
+    variables({
+      :dbuser => node[:users].first[:username],
+      :dbpass => node[:users].first[:password]
+    })
+  end
+
   directory "/mnt/tmp" do
     action :create
     owner "postgres"
@@ -46,7 +96,7 @@ if ['db_slave'].include?(node[:instance_role])
   end
 
   execute "start_rsync" do
-    command "su -c 'cd /db/postgresql/8.3;rsync -avPz --exclude .svn --exclude recovery.conf --exclude pg_xlog #{node.engineyard.environment.db_host}:/db/postgresql/8.3/data/ /db/postgresql/8.3/data/' - postgres"
+    command "su -c 'cd /db/postgresql/8.3;rsync -avPz --exclude .svn --exclude postgresql.conf --exclude recovery.conf --exclude pg_xlog #{node.engineyard.environment.db_host}:/db/postgresql/8.3/data/ /db/postgresql/8.3/data/' - postgres"
     only_if { FileTest.exists?("/db/resync") }
   end
 
@@ -75,7 +125,7 @@ if ['db_slave'].include?(node[:instance_role])
   end
 
   execute "start_postgres" do
-    command "/etc/init.d/postgresql-8.3 start"
+    command "cd /db/postgresql/8.3/data;ln -sfv /db/postgresql/8.3/postgresql.conf . && cd /db/postgresql/8.3/data;ln -sfv /db/postgresql/8.3/pg_hba.conf . && /etc/init.d/postgresql-8.3 start"
     only_if { FileTest.exists?("/db/resync") }
   end
 
