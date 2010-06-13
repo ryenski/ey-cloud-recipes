9a225f83efd2e0b53826f3cc03545760452e3edb
Scott M. Likens <slikens@engineyard.com>
2010-06-13 10:49:55 -0700

    - Client did not run the way I wanted to on the app_master instance.
    Signed-off-by: Scott M. Likens <slikens@engineyard.com>

--- cookbooks/postgres/recipes/default.rb
+++ cookbooks/postgres/recipes/default.rb
@@ -1,8 +1,12 @@
+require_recipe 'postgres::client'
+
 if ['solo', 'db_master'].include?(node[:instance_role])
   require_recipe 'postgres::server'
   require_recipe 'postgres::eybackup'
 end
-require_recipe 'postgres::client'
+if ['solo', 'db_slave'].include?(node[:instance_role])
+  require_recipe 'postgres::server'
+end
 if ['solo', 'app_master', 'app', 'util'].include?(node[:instance_role])
   require_recipe 'postgres::database'
 end
